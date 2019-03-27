8df0d9efb7bea06595f11d046598d5504271f1dd
visualstudio <visualstudio@122-PC39>
2019-03-27 10:18:02 -0400

    commit 10:20

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp18
+namespace LearningAsync
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            Task.Run(()=>{
+                Thread.Sleep(3000);
+                Console.WriteLine("Download Complete");
+            });
         }
     }
 }
